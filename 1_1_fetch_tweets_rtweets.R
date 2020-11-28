ff75ec0682367a1dafd8aa1d45ee07b26f1e9e6a
jaNGOB <gobeli@hotmail.ch>
2020-11-28 18:06:35 +0100

    Add files via upload

--- 1_1_fetch_tweets_rtweets.R
+++ 1_1_fetch_tweets_rtweets.R
@@ -7,10 +7,13 @@ if (!requireNamespace("httpuv", quietly = TRUE)) {
 }
 
 name <- 'ProgrammingUSI'
+app_id <- '19421261'
 api <- 'RxEdqHZMSd6LYzhxADq9CQINU'
 secret <- 'xQaRGBeBWEr51P6ik9Hf8R7GYY1z5BWoUJAfrcxptQRSltJz8h'
 access_token <- '1722107810-OuMtkX9MRFg0lUU5pOUxqeaoONuJXAdQXdnig5L'
 access_token_secret <- '9Afe1KSumpG1d5ngAwox6WArvk5jU1p0d4yvHGNe77q5D'
+
+setup_twitter_oauth(api ,secret,access_token ,access_secret)
 
 
 token <- create_token(
@@ -20,10 +23,19 @@ token <- create_token(
   access_token = access_token,
   access_secret = access_token_secret)
 
-team_rstats <- search_tweets("#rstats", n = 500)
-team_rstats
 
-aapl_tweets <- search_tweets("$AAPL", n = 25000, retryonratelimit=TRUE)
+tickers <- c('$AAPL', '$MSFT', '$GE')
+tickers_clean <- c('AAPl', 'MSFT', 'GE')
 
+flw <- vector("list", length(tickers_clean))
 
-ts_plot(aapl_tweets)
+for (n in 1:length(tickers)){
+  flw[[n]] <- search_tweets(tickers[n], n = 100, retryonratelimit=FALSE)
+}
+
+saveRDS(flw, file="C:/Users/jango/Documents/USI/1. Semester/Programming_in_Finance_1/Twitter2Money/tweets.RData")
+
+jiji <- readRDS("C:/Users/jango/Documents/USI/1. Semester/Programming_in_Finance_1/Twitter2Money/tweets.RData")
+
+msft_tweets <- search_fullarchive("$MSFT", fromDate = start, toDate = end, env_name = name, token = token)
+aapl_tweets <- search_tweets("$AAPL", n = 100000, retryonratelimit=TRUE)
